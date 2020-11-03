f01733b11cda7763ec98e1f9c1d3a0d0e5325804
ayumu5826 <61813232+ayumu5826@users.noreply.github.com>
2020-11-03 16:02:26 +0800

    角色與選單腳本

--- ASIA_2DGame/Assets/Scripts/MenuManager.cs
+++ ASIA_2DGame/Assets/Scripts/MenuManager.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    
-    void SrartGame()
-    {
+    //註解
+    //按鈕跟程式溝通
+    //私人 private
+    //公開 public
+    //void無類型
 
+    public void SrartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡一");
     }
 
-    void QuitGame()
+    public void QuitGame()
     {
-
+        print("離開遊戲");
+        Application.Quit();
     }
 }
