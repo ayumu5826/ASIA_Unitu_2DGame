38a145f045e177a90d465b4a2f3a5126f780db75
ayumu5826 <61813232+ayumu5826@users.noreply.github.com>
2020-11-10 15:59:34 +0800

    1110

--- ASIA_2DGame/Assets/Scripts/Player.cs
+++ ASIA_2DGame/Assets/Scripts/Player.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject Final;
+    public Text textcount;
+    int Count=0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門");
-        Final.SetActive(true);
+        if(collision.name=="傳送門")
+        {
+            print("碰到傳送門");
+            Final.SetActive(true);
+        }
+
+        if (collision.tag == "櫻桃")
+        {
+            Destroy(collision.gameObject);
+            Count++;
+            textcount.text = "櫻桃數量：" + Count;
+        }
     }
 
     
